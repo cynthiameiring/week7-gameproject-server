ccb06e683e95753e939ff040295567780e651840
cynthiameiring <cynthia.meiring@hotmail.com>
2019-11-18 11:29:30 +0100

    add login endpoint

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ app.use(corsMiddleware);
 const userRouter = require("./user/router");
 app.use(userRouter);
 
-// const userRouter = require("./user/router");
-// app.use(userRouter);
-
-// const authRouter = require("./auth/router");
-// app.use(authRouter);
+const authRouter = require("./auth/router");
+app.use(authRouter);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
